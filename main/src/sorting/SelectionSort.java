2a4a73aca83984ef0bd61cce59a0b9f14d8b035e
Mgrdich <mgotm13@gmail.com>
2022-09-20 11:23:37 +0400

    Add infrastructure for new sorts

--- main/src/sorting/SelectionSort.java
+++ main/src/sorting/SelectionSort.java
@@ -1,4 +1,16 @@
 package sorting;
 
+/***
+ * Selection sort works in the following way<br></>
+ * 1. Find the smallest element , exchange it with the first element in the array <br></>
+ * 2. Find the second smallest element , exchange it with the second element in the array <br></>
+ * 3. Continue in this way until the whole array is sorted <br></>
+ *
+ * Algorithmic Complexity and Analysis
+ * O(n^2)
+ * */
 public class SelectionSort {
+    public static void sort(int[] arr){
+
+    }
 }
