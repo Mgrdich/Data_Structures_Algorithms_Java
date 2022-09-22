36d037b2470696b15b4cd6a79f60798ee8ad7d77
Mgrdich <mgotm13@gmail.com>
2022-09-22 11:31:26 +0400

    add merge sor to the Soring algorithm

--- main/src/sorting/MergeSort.java
+++ main/src/sorting/MergeSort.java
@@ -1,4 +1,44 @@
 package sorting;
 
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+
+/**
+ * It uses recursion, and it divides to conquer the sorting
+ * its running time is O(nlog(n)) for all the cases
+ * */
 public class MergeSort {
+    public static void sort(Integer[] arr) {
+        int n = arr.length;
+
+        if (n < 2) return;
+
+        int mid = n / 2;
+
+        // TODO create a Util alternative Arrays copy of range
+        Integer[] arr1 = Arrays.copyOfRange(arr, 0, mid);
+        Integer[] arr2 = Arrays.copyOfRange(arr, mid, n);
+
+        sort(arr1);
+        sort(arr2);
+
+        merge(arr1, arr2, arr);
+    }
+
+    private static void merge(Integer[] arr1, Integer[] arr2, Integer[] arr) {
+        if (arr1.length + arr2.length != arr.length) {
+            throw new InvalidParameterException("the parameters length are not equivalent");
+        }
+
+        int j = 0;
+        int i = 0;
+
+        while (i + j < arr.length) {
+            if (j == arr2.length || (i < arr1.length && arr1[i] < arr2[j])) {
+                arr[i + j] = arr1[i++];
+            } else {
+                arr[i + j] = arr2[j++];
+            }
+        }
+    }
 }
