8e2dddd7308e9d9ad96e19eb34ec9e974dfefce0
Mgrdich <mgotm13@gmail.com>
2022-09-25 22:50:00 +0400

    Finalize CircularlyLinkedList implementations

---
+++ main/src/data_structure/CircularlyLinkedList.java
@@ -0,0 +1,63 @@
+package data_structure;
+
+public class CircularlyLinkedList<E> {
+
+    private SinglyLinkedListNode<E> tail;
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        if (isEmpty()) return null;
+        return tail.getNext().getElement();
+    }
+
+    public E last() {
+        if (isEmpty()) return null;
+        return tail.getElement();
+    }
+
+    public void rotate() {
+        if (!isEmpty()) {
+            tail = tail.getNext();
+        }
+    }
+
+    public void addFirst(E element) {
+        SinglyLinkedListNode<E> newNode = new SinglyLinkedListNode<>(element, null);
+        if (isEmpty()) {
+            tail = newNode;
+        } else {
+            newNode.setNext(tail.getNext());
+        }
+        tail.setNext(newNode);
+        size++;
+    }
+
+    public void addLast(E element) {
+        addFirst(element);
+        rotate();
+    }
+
+    public E removeFirst(E element) {
+        if (isEmpty()) return null;
+        SinglyLinkedListNode<E> head = tail.getNext();
+        if (head == tail) {
+            tail = null;
+        } else {
+            tail.setNext(head.getNext());
+        }
+        size--;
+        return head.getElement();
+    }
+
+    public void print() {
+
+    }
+}
