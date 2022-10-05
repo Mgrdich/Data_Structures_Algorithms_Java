67ac52746523ae4c6ff8473b7883561c4b113e94
Mgrdich <mgotm13@gmail.com>
2022-10-06 01:29:05 +0400

    add array deque functionality

--- main/src/data_structure/ArrayDeque.java
+++ main/src/data_structure/ArrayDeque.java
@@ -3,37 +3,81 @@ package data_structure;
 import adt.Deque;
 
 public class ArrayDeque<E> implements Deque<E> {
+    public static int CAPACITY = 1000;
     private int first = 0;
+
+    private int last = 0;
     private int size = 0;
 
-    @Override
-    public void addFirst(E element) {
+    private final E[] deque;
 
+    public ArrayDeque() {
+        this(CAPACITY);
+    }
+
+    public ArrayDeque(int capacity) {
+        deque = (E[]) new Object[capacity];
     }
 
     @Override
-    public void addLast(E element) {
+    public void addFirst(E element) throws IllegalStateException {
+        if (size == size()) throw new IllegalStateException("Out of memory");
 
+        int availableIndex = (first) % deque.length;
+        deque[availableIndex] = element;
+        last = (first + 1) % deque.length;
+        size++;
+    }
+
+    @Override
+    public void addLast(E element) throws IllegalStateException {
+        if (size == size()) throw new IllegalStateException("Out of memory");
+
+        int availableIndex = (last) % deque.length;
+        deque[availableIndex] = element;
+        last = (last + 1) % deque.length;
+        size++;
     }
 
     @Override
     public E removeFirst() {
-        return null;
+        if (isEmpty()) return null;
+
+        E element = deque[first];
+        deque[first] = null; // garbage collection
+
+        first = (first + 1) % deque.length;
+        size--;
+
+        return element;
     }
 
     @Override
     public E removeLast() {
-        return null;
+        if (isEmpty()) return null;
+
+        E element = deque[last];
+        deque[last] = null; // garbage collection
+
+        last = last - 1;
+        last = last == -1 ? deque.length - 1 : last % deque.length;
+        size--;
+
+        return element;
     }
 
     @Override
     public E first() {
-        return null;
+        if (isEmpty()) return null;
+
+        return deque[first];
     }
 
     @Override
     public E last() {
-        return null;
+        if (isEmpty()) return null;
+
+        return deque[last];
     }
 
     @Override
