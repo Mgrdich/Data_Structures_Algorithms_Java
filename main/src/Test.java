fc765d7c917ea294c9a975abb672ca03c04d5d12
Mgrdich <mgotm13@gmail.com>
2022-10-07 01:03:19 +0400

    CircularlyDoublyLinkedList code rotate finalization

--- main/src/Test.java
+++ main/src/Test.java
@@ -3,8 +3,8 @@ import data_structure.*;
 
 public class Test {
     public static void main(String[] args) {
-        Deque<Integer> dq = new ArrayDeque<>();
-        Deque<Integer> dq1 = new LinkedDeque<>();
+//        Deque<Integer> dq = new ArrayDeque<>();
+//        Deque<Integer> dq1 = new LinkedDeque<>();
 
 //        dq.addFirst(1);
 //        dq.addLast(2);
@@ -57,41 +57,55 @@ public class Test {
 //        System.out.println(dq1.last());
 
 
-        dq.addLast(5);
-        dq1.addLast(5);
+//        dq.addLast(5);
+//        dq1.addLast(5);
+//
+//        dq.addFirst(3);
+//        dq1.addFirst(3);
+//
+//        dq.addFirst(7);
+//        dq1.addFirst(7);
+//
+//        System.out.println("--");
+//        System.out.println(dq.first() + " " + dq1.first());
+//
+//        System.out.println("--");
+//        System.out.println(dq.removeLast() + " " + dq1.removeLast());
+//
+//        System.out.println("--");
+//        System.out.println(dq.size() + " " + dq1.size());
+//
+//        System.out.println("--");
+//        System.out.println(dq.removeLast() + " " + dq1.removeLast());
+//
+//        System.out.println("--");
+//        System.out.println(dq.removeFirst() + " " + dq1.removeFirst());
+//
+//        System.out.println("--");
+//        dq.addFirst(6);
+//        dq1.addFirst(6);
+//        System.out.println(dq.last() + " " + dq1.last());
+//
+//        System.out.println("--");
+//        dq.addFirst(8);
+//        dq1.addFirst(8);
+//        System.out.println(dq.isEmpty() + " " + dq1.isEmpty());
+//
+//        System.out.println("--");
+//        System.out.println(dq.last() + " " + dq1.last());
 
-        dq.addFirst(3);
-        dq1.addFirst(3);
 
-        dq.addFirst(7);
-        dq1.addFirst(7);
+        CircularlyDoublyLinkedList<Integer> db = new CircularlyDoublyLinkedList<>();
 
-        System.out.println("--");
-        System.out.println(dq.first() + " " + dq1.first());
+        db.addFirst(1);
+        db.addLast(2);
+        db.addLast(3);
+        db.addLast(4);
 
-        System.out.println("--");
-        System.out.println(dq.removeLast() + " " + dq1.removeLast());
-
-        System.out.println("--");
-        System.out.println(dq.size() + " " + dq1.size());
-
-        System.out.println("--");
-        System.out.println(dq.removeLast() + " " + dq1.removeLast());
-
-        System.out.println("--");
-        System.out.println(dq.removeFirst() + " " + dq1.removeFirst());
-
-        System.out.println("--");
-        dq.addFirst(6);
-        dq1.addFirst(6);
-        System.out.println(dq.last() + " " + dq1.last());
-
-        System.out.println("--");
-        dq.addFirst(8);
-        dq1.addFirst(8);
-        System.out.println(dq.isEmpty() + " " + dq1.isEmpty());
-
-        System.out.println("--");
-        System.out.println(dq.last() + " " + dq1.last());
+        db.print();
+        db.rotate();
+        db.print();
+        System.out.println(db.first());
+        System.out.println(db.last());
     }
 }
--- main/src/data_structure/CircularlyDoublyLinkedList.java
+++ main/src/data_structure/CircularlyDoublyLinkedList.java
@@ -1,8 +1,14 @@
 package data_structure;
 
 public class CircularlyDoublyLinkedList<E> {
-    private DoubleLinkedListNode<E> sentinal;
+    private final DoubleLinkedListNode<E> sentinel;
     private int size = 0;
+
+    public CircularlyDoublyLinkedList() {
+        sentinel = new DoubleLinkedListNode<>(null, null, null);
+        sentinel.setNext(sentinel);
+        sentinel.setPrev(sentinel);
+    }
 
     public int getSize() {
         return size;
@@ -12,5 +18,57 @@ public class CircularlyDoublyLinkedList<E> {
         return getSize() == 0;
     }
 
-    public void rotate() {}
+    public void rotate() {
+
+        DoubleLinkedListNode<E> nextNode = sentinel.getNext();
+        DoubleLinkedListNode<E> prevNode = sentinel.getPrev();
+        DoubleLinkedListNode<E> doubleNextNode = nextNode.getNext();
+
+        nextNode.setPrev(prevNode);
+        prevNode.setNext(nextNode);
+
+        nextNode.setNext(sentinel);
+        sentinel.setPrev(nextNode);
+
+        doubleNextNode.setPrev(sentinel);
+        sentinel.setNext(doubleNextNode);
+    }
+
+    public E first() {
+        if (isEmpty()) return null;
+
+        return sentinel.getNext().getElement();
+    }
+
+    public E last() {
+        if (isEmpty()) return null;
+
+        return sentinel.getPrev().getElement();
+    }
+
+    public void addFirst(E element) {
+        addBetween(element, sentinel, sentinel.getNext());
+    }
+
+    public void addLast(E element) {
+        addBetween(element, sentinel.getPrev(), sentinel);
+    }
+
+    private void addBetween(E element, DoubleLinkedListNode<E> predecessor, DoubleLinkedListNode<E> successor) {
+        DoubleLinkedListNode<E> newNode = new DoubleLinkedListNode<>(element, predecessor, successor);
+
+        predecessor.setNext(newNode);
+        successor.setPrev(newNode);
+        size++;
+    }
+
+    public void print() {
+        DoubleLinkedListNode<E> curr = sentinel.getNext();
+
+        while (curr != sentinel) {
+            System.out.print(curr.getElement() + " ");
+            curr = curr.getNext();
+        }
+        System.out.println();
+    }
 }
