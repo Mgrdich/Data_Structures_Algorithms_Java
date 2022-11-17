b650fce387715a3aba9bd10bdd1e5f68762eec4f
Mgrdich <mgotm13@gmail.com>
2022-11-18 01:34:53 +0400

    add HeapPriorityQueue

---
+++ main/src/data_structure/HeapPriorityQueue.java
@@ -0,0 +1,104 @@
+package data_structure;
+
+import adt.Entry;
+import adt.List;
+
+import java.util.Comparator;
+
+public class HeapPriorityQueue<K, V> extends AbstractPriorityQueue<K, V> {
+    List<Entry<K, V>> heap = new ArrayList<>();
+
+    public HeapPriorityQueue() {
+        super();
+    }
+
+    public HeapPriorityQueue(Comparator<K> comparator) {
+        super(comparator);
+    }
+
+    protected int parent(int j) {
+        return (j - 1) / 2;
+    }
+
+    protected int left(int j) {
+        return 2 * j + 1;
+    }
+
+    protected int right(int j) {
+        return 2 * j + 2;
+    }
+
+    protected boolean hasLeft(int j) {
+        return left(j) < heap.size();
+    }
+
+    protected boolean hasRight(int j) {
+        return right(j) < heap.size();
+    }
+
+    protected void swap(int i, int j) {
+        Entry<K, V> temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
+
+    protected void upHeap(int j) {
+        while (j > 0) {
+            int p = parent(j);
+            if (compare(heap.get(j), heap.get(p)) >= 0) break;
+
+            swap(j, p);
+            j = p;
+        }
+    }
+
+    protected void downHeap(int j) {
+        while (hasLeft(j)) {
+            int leftIndex = left(j);
+            int smallChildIndex = leftIndex;
+
+            if (hasRight(j)) {
+                int rightIndex = right(j);
+                if (compare(heap.get(leftIndex), heap.get(rightIndex)) > 0) {
+                    smallChildIndex = rightIndex;
+                }
+            }
+
+            if (compare(heap.get(smallChildIndex), heap.get(j)) >= 0) break;
+
+            swap(j, smallChildIndex);
+            j = smallChildIndex;
+        }
+    }
+
+    @Override
+    public int size() {
+        return heap.size();
+    }
+
+    @Override
+    public Entry<K, V> min() {
+        if (isEmpty()) return null;
+        return heap.get(0);
+    }
+
+    @Override
+    public Entry<K, V> removeMin() {
+        if (isEmpty()) return null;
+
+        Entry<K, V> min = heap.get(0);
+        swap(0, heap.size() - 1);
+        heap.remove();
+        downHeap(0);
+        return min;
+    }
+
+    @Override
+    public Entry<K, V> insert(K key, V value) throws IllegalArgumentException {
+        checkKey(key);
+        Entry<K, V> newEntry = new PQEntry<>(key, value);
+        heap.add(newEntry);
+        upHeap(heap.size() - 1);
+        return newEntry;
+    }
+}
