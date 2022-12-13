b72c65fbafc845524bc2b52f43689bc942477f4e
Mgrdich <mgotm13@gmail.com>
2022-12-13 21:29:42 +0400

    BSTIterator code initialization

--- main/src/Problems/BSTIterator.java
+++ main/src/Problems/BSTIterator.java
@@ -4,19 +4,46 @@ import data_structure.LinkedBinaryTreeNode;
 
 import java.util.Iterator;
 
-public class BSTIterator<E> implements Iterator<E> {
+public class BSTIterator implements Iterator<Integer> {
 
-    public BSTIterator(LinkedBinaryTreeNode<E> root) {
+    private LinkedBinaryTreeNode<Integer> next;
 
+
+    public BSTIterator(LinkedBinaryTreeNode<Integer> root) {
+        next = min(root);
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return next != null;
     }
 
     @Override
-    public E next() {
-        return null;
+    public Integer next() {
+        int ret = next.getElement();
+        next = inOrderAfter(next);
+        return ret;
+    }
+
+    public static LinkedBinaryTreeNode<Integer> inOrderAfter(LinkedBinaryTreeNode<Integer> node) {
+
+        if (node.getRight() != null) {
+            return min(node.getRight());
+        }
+
+        LinkedBinaryTreeNode<Integer> parentNode = node.getParent();
+        while (parentNode != null && node == parentNode.getRight()) {
+            node = parentNode;
+            parentNode = parentNode.getParent();
+        }
+        return parentNode;
+    }
+
+    public static LinkedBinaryTreeNode<Integer> min(LinkedBinaryTreeNode<Integer> node) {
+        // left most leaf
+        while (node.getLeft() != null) {
+            node = node.getLeft();
+        }
+        return node;
     }
 }
