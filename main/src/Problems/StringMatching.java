4387d1469ae004697b281e3798dbaf37f2d5da7d
Mgrdich <mgotm13@gmail.com>
2023-11-11 13:32:52 +0400

    add String Matching code modifications

--- main/src/Problems/StringMatching.java
+++ main/src/Problems/StringMatching.java
@@ -1,46 +1,52 @@
 package Problems;
 
+
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StringMatching {
     public static void main(String[] args) {
-        char[] alphabet = {'a', 'b'};
         String pattern = "aabaabaaab";
         String text = "aaabaabaaabaabaaabab";
 
+        Map<Character, Integer> alphabet = createAlphabet(new char[]{'a', 'b'});
+
+        System.out.println("text: "+ text);
+        System.out.println("pattern: "+ pattern);
         int[][] transitionFunction = computeTransitionFunction(pattern, alphabet);
         System.out.println(Arrays.deepToString(transitionFunction).replace("], ", "]\n"));
 
-        finiteAutomationMatching(text, transitionFunction, pattern.length(), alphabet);
+        finiteAutomatonMatching(text, transitionFunction, pattern.length(), alphabet);
 
         System.out.println();
     }
 
-    public static int[][] computeTransitionFunction(String pattern, char[] alphabet) {
+    public static int[][] computeTransitionFunction(String pattern, Map<Character, Integer> alphabet) {
         int m = pattern.length();
-        int[][] delta = new int[m + 1][alphabet.length];
+        int[][] delta = new int[m + 1][alphabet.size()];
 
         for (int q = 0; q <= m; q++) {
-            for (int i = 0; i < alphabet.length; i++) {
-                char a = alphabet[i];
+            for (Map.Entry<Character, Integer> entry : alphabet.entrySet()) {
+                char a = entry.getKey();
                 int k = Math.min(q + 2, m + 1);
                 do {
                     k--;
                 } while (k > 0 && !pattern.substring(0, q).concat(String.valueOf(a)).endsWith(pattern.substring(0, k)));
-                delta[q][i] = k;
+                delta[q][entry.getValue()] = k;
             }
         }
 
         return delta;
     }
 
-    public static void finiteAutomationMatching(String text, int[][] delta, int m, char[] alphabet) {
+    public static void finiteAutomatonMatching(String text, int[][] delta, int m, Map<Character, Integer> alphabet) {
         int n = text.length();
         int q = 0;
 
         for (int i = 0; i < n; i++) {
             char a = text.charAt(i);
-            q = delta[q][getIndex(a, alphabet)];
+            q = delta[q][alphabet.get(a)];
 
             if (q == m) {
                 System.out.println("Pattern occurs at shift " + (i - m + 1));
@@ -48,12 +54,12 @@ public class StringMatching {
         }
     }
 
-    private static int getIndex(char c, char[] alphabet) {
+    private static Map<Character, Integer> createAlphabet(char[] alphabet) {
+        Map<Character, Integer> alphabetMapped = new HashMap<>(alphabet.length);
+
         for (int i = 0; i < alphabet.length; i++) {
-            if (alphabet[i] == c) {
-                return i;
-            }
+            alphabetMapped.put(alphabet[i], i);
         }
-        return -1;
+        return alphabetMapped;
     }
 }
